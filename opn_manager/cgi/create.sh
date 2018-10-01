99ab24bc614864d069d249ca6c87b4008e1fd399
Mathias Neerup <mane@mmmi.sdu.dk>
2018-10-01 12:50:12 +0200

    Update to use nginx as proxy instead of redirecting to container ip

--- opn_manager/cgi/create.sh
+++ opn_manager/cgi/create.sh
@@ -2,13 +2,21 @@
 echo "Content-type: text/html"
 echo ''
 
+echo "<pre>"
+env
+echo "<pre>"
+# Get name of docker-compose network
+NETWORK=$(docker network ls | grep opn_manager | awk '{print $2}')
+
+echo "network is: $NETWORK</br>";
+
 # Get unique ID of newly created container
-ID=$(docker run -d opn_backend)
+ID=$(docker run --network $NETWORK -d opn_backend | cut -b1-12)
 
 # Get IP of newly created container from ID
-IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $ID)
+#IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $ID)
 
-echo "Container started with ID: $ID and IP: $IP</br>"
+echo "Container started with ID: $ID</br>"
 echo "Browser will be redirected in 3 seconds..."
-echo "<META HTTP-EQUIV=\"Refresh\" Content=\"3; URL=http://$IP/cgi-bin/welcome.sh\">"
+echo "<META HTTP-EQUIV=\"Refresh\" Content=\"3; URL=http://$HTTP_HOST/$ID/cgi-bin/welcome.sh\">"
 
--- opn_manager/cgi/list.sh
+++ opn_manager/cgi/list.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Content-type: text/html"
 echo ''
+echo "<meta http-equiv="refresh" content="1"> <!-- See the difference? -->"
 echo "<pre>"
 docker ps
-echo "</pre>"
+echo "</pre></br>"
+echo "<a href=\"/cgi-bin/create.sh\">Create container</a>"
